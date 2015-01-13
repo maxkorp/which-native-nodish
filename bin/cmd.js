1398734aa89e1ae6f1a45355d663b2d5088511ff
Maximiliano Korp <maxkorp@8bytealchemy.com>
2015-01-12 17:13:25 -0700

    split binary out

---
+++ bin/cmd.js
@@ -0,0 +1,22 @@
+var fnw = require('../');
+var directory = (process.argv.length > 2 && process.argv[2]) || process.cwd();
+
+fnw(directory)
+  .then(
+    function(results) {
+      if (!results.root) {
+        console.error("could not find a parent package for the directory '" + directory + "'");
+      }
+      else if (!results.nwVersion) {
+        console.log("root package at '" + results.root
+        + "' does not use node-webkit");
+      }
+      else {
+        console.log("root package at '" + results.root + "' uses node-webkit v"
+        + results.nwVersion);
+      }
+    },
+    function(err) {
+      console.error(err.message);
+    }
+  );
--- index.js
+++ index.js
@@ -56,24 +56,3 @@ function findRoot (toCheck) {
     find(toCheck);
   });
 }
-
-if (require.main === module) {
-  var directory = (process.argv.length > 2 && process.argv[2]) || process.cwd();
-  module.exports(directory)
-    .then(function(results) {
-      if (!results.root) {
-        console.error("could not find a parent package for the directory '" + directory + "'");
-      }
-      else if (!results.nwVersion) {
-        console.log("root package at '" + results.root
-          + "' does not use node-webkit");
-      }
-      else {
-        console.log("root package at '" + results.root + "' uses node-webkit v"
-          + results.nwVersion);
-      }
-    },
-    function(err) {
-      console.error(err.message);
-    });
-}
